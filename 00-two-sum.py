f659a500ab7fab3a9eb8e4854cac785f766c71ad
Daniel Ni <danielmichaelni@gmail.com>
2017-10-16 00:22:06 -0700

    fix quotes in two sum problem statement

--- 00-two-sum.py
+++ 00-two-sum.py
@@ -1,8 +1,8 @@
 """
 You are given an array of n integers and a number k. Determine whether there is
-a pair of elements in the array that sums to exactly k. For example, given the
-array [1, 3, 7] and k = 8, the answer is “yes,” but given k = 6 the answer is
-“no.”
+a pair of elements in the array that sums to exactly k. For example, given the
+array [1, 3, 7] and k = 8, the answer is "yes", but given k = 6 the answer is
+"no."
 
 Problem provided by: https://web.stanford.edu/class/cs9/lectures/04/Two-Sum.pdf
 """
